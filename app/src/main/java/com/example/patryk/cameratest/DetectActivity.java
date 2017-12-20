c315f866ac18cd7ea8adf08205132d4e170b70d7
medium007 <patryk.milewczyk95@gmail.com>
2017-12-20 02:25:08 +0100

    Detector

--- app/src/main/java/com/example/patryk/cameratest/DetectActivity.java
+++ app/src/main/java/com/example/patryk/cameratest/DetectActivity.java
@@ -1,24 +1,13 @@
 package com.example.patryk.cameratest;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-
-import org.tensorflow.DataType;
-import org.tensorflow.Graph;
-import org.tensorflow.Output;
-import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
-import org.tensorflow.types.UInt8;
 
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
@@ -26,37 +15,25 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-import static java.lang.String.valueOf;
 
 public class DetectActivity extends AppCompatActivity {
 
-//    private static final int MAX_RESULTS = 100;
-//    private String inputName;
-//    private int inputSize;
-//    private AssetManager assetManager;
-//    private String modelFilename;
-//    private Vector<String> labels = new Vector<String>();
-//    private int[] intValues;
-//    private byte[] byteValues;
-//    private float[] outputLocations;
-//    private float[] outputScores;
-//    private float[] outputClasses;
-//    private float[] outputNumDetections;
-//    private String[] outputNames;
-//    private static final int INPUT_SIZE = 224;
-//    private boolean logStats = false;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-//    static {
-//        //System.load("src/main/jniLibs/armeabi-v7a/libtensorflow_inference.so");
-////        System.loadLibrary("libtensorflow_inference");
-//    }
+    private static final String INPUT_NAME = "input";
+    private static final String OUTPUT_NAME = "output";
+
+    private static final String MODEL_FILE = "file:///android_asset/frozen_inference_graph.pb";
+    private static final String LABEL_FILE = "file:///android_asset/graph_label_strings.txt";
+
+    private static final int INPUT_SIZE = 960;
+
+    private Classifier classifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,13 +41,8 @@ public class DetectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detect);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public void dispatchTakePictureIntent(View view) {
-/*        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }*/
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         Bitmap bitmap;
         try {
@@ -102,16 +74,10 @@ public class DetectActivity extends AppCompatActivity {
     }
 
     public void detect(Bitmap bitmap) {
-        boolean logStats = false;
-        String[] outputNames = null;
-        String modelName = "frozen_inference_graph.pb";
         EditText editText = (EditText) findViewById(R.id.editText);
 
-        TensorFlowInferenceInterface d = new TensorFlowInferenceInterface(getAssets(), modelName);
-        byte[] byteArray = readAllBytes(bitmap);
-        d.feedString(modelName, byteArray);
-        d.run(outputNames, logStats);
-
+        String res = initTensorFlowAndLoadModel(readAllBytes(bitmap));
+        editText.setText(res);
     }
 
     private static byte[] readAllBytes(Bitmap bitmap) {
@@ -121,212 +87,30 @@ public class DetectActivity extends AppCompatActivity {
         return byteArray;
     }
 
-/*    public void detect(Bitmap bitmap) {
-        String modelName = "person_ig";
-        EditText editText = (EditText) findViewById(R.id.editText);
+    private String initTensorFlowAndLoadModel(byte[] pixels) {
 
-        byte[] graphDef = readAllBytesOrExit(Paths.get("file:///android_asset/", "frozen_inference_graph.pb"));
-        String label = "personPM";
-
-        byte[] imageBytes = readAllBytesOrExit(bitmap);
-
-        try (Tensor<Float> image = constructAndExecuteGraphToNormalizeImage(imageBytes)) {
-            float[] labelProbabilities = executeInceptionGraph(graphDef, image);
-            int bestLabelIdx = maxIndex(labelProbabilities);
-            editText.setText(String.format("BEST MATCH: %s (%.2f%% likely)",
-                    label,
-                    labelProbabilities[bestLabelIdx] * 100f));
-        }
-    }*/
-
-/*    private static Tensor<Float> constructAndExecuteGraphToNormalizeImage(byte[] imageBytes) {
-        try (Graph g = new Graph()) {
-            GraphBuilder b = new GraphBuilder(g);
-            // Some constants specific to the pre-trained model at:
-            // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
-            //
-            // - The model was trained with images scaled to 224x224 pixels.
-            // - The colors, represented as R, G, B in 1-byte each were converted to
-            //   float using (value - Mean)/Scale.
-            final int H = 224;
-            final int W = 224;
-            final float mean = 117f;
-            final float scale = 1f;
-
-            // Since the graph is being constructed once per execution here, we can use a constant for the
-            // input image. If the graph were to be re-used for multiple input images, a placeholder would
-            // have been more appropriate.
-            final Output<String> input = b.constant("input", imageBytes);
-            final Output<Float> output =
-                    b.div(
-                            b.sub(
-                                    b.resizeBilinear(
-                                            b.expandDims(
-                                                    b.cast(b.decodeJpeg(input, 3), Float.class),
-                                                    b.constant("make_batch", 0)),
-                                            b.constant("size", new int[] {H, W})),
-                                    b.constant("mean", mean)),
-                            b.constant("scale", scale));
-            try (Session s = new Session(g)) {
-                return s.runner().fetch(output.op().name()).run().get(0).expect(Float.class);
-            }
-        }
-    }
-
-    private static float[] executeInceptionGraph(byte[] graphDef, Tensor<Float> image) {
-        try (Graph g = new Graph()) {
-            g.importGraphDef(graphDef);
-            try (Session s = new Session(g);
-                 Tensor<Float> result =
-                         s.runner().feed("input", image).fetch("output").run().get(0).expect(Float.class)) {
-                final long[] rshape = result.shape();
-                if (result.numDimensions() != 2 || rshape[0] != 1) {
-                    throw new RuntimeException(
-                            String.format(
-                                    "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
-                                    Arrays.toString(rshape)));
-                }
-                int nlabels = (int) rshape[1];
-                return result.copyTo(new float[1][nlabels])[0];
-            }
-        }
-    }
-
-    private static int maxIndex(float[] probabilities) {
-        int best = 0;
-        for (int i = 1; i < probabilities.length; ++i) {
-            if (probabilities[i] > probabilities[best]) {
-                best = i;
-            }
-        }
-        return best;
-    }
-
-    private static byte[] readAllBytesOrExit(Path path) {
+        String value = "";
         try {
-            return Files.readAllBytes(path);
-        } catch (IOException e) {
-            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
-            System.exit(1);
+            classifier = ImageDetector.create(
+                    getAssets(),
+                    MODEL_FILE,
+                    LABEL_FILE,
+                    INPUT_SIZE,
+                    INPUT_NAME,
+                    OUTPUT_NAME);
+        } catch (final Exception e) {
+            throw new RuntimeException("Error initializing TensorFlow!", e);
         }
-        return null;
+
+
+        List<Classifier.Recognition> results;
+        results = classifier.recognizeImage(pixels);
+
+        if (results.size() > 0) {
+            value = " Result: " +results.get(0).getTitle();
+        }
+
+        classifier.close();
+        return value;
     }
-
-    private static byte[] readAllBytesOrExit(Bitmap bitmap) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        return byteArray;
-    }
-
-    static class GraphBuilder {
-        GraphBuilder(Graph g) {
-            this.g = g;
-        }
-
-        Output<Float> div(Output<Float> x, Output<Float> y) {
-            return binaryOp("Div", x, y);
-        }
-
-        <T> Output<T> sub(Output<T> x, Output<T> y) {
-            return binaryOp("Sub", x, y);
-        }
-
-        <T> Output<Float> resizeBilinear(Output<T> images, Output<Integer> size) {
-            return binaryOp3("ResizeBilinear", images, size);
-        }
-
-        <T> Output<T> expandDims(Output<T> input, Output<Integer> dim) {
-            return binaryOp3("ExpandDims", input, dim);
-        }
-
-        <T, U> Output<U> cast(Output<T> value, Class<U> type) {
-            DataType dtype = DataType.fromClass(type);
-            return g.opBuilder("Cast", "Cast")
-                    .addInput(value)
-                    .setAttr("DstT", dtype)
-                    .build()
-                    .<U>output(0);
-        }
-
-        Output<UInt8> decodeJpeg(Output<String> contents, long channels) {
-            return g.opBuilder("DecodeJpeg", "DecodeJpeg")
-                    .addInput(contents)
-                    .setAttr("channels", channels)
-                    .build()
-                    .<UInt8>output(0);
-        }
-
-        <T> Output<T> constant(String name, Object value, Class<T> type) {
-            try (Tensor<T> t = Tensor.<T>create(value, type)) {
-                return g.opBuilder("Const", name)
-                        .setAttr("dtype", DataType.fromClass(type))
-                        .setAttr("value", t)
-                        .build()
-                        .<T>output(0);
-            }
-        }
-        Output<String> constant(String name, byte[] value) {
-            return this.constant(name, value, String.class);
-        }
-
-        Output<Integer> constant(String name, int value) {
-            return this.constant(name, value, Integer.class);
-        }
-
-        Output<Integer> constant(String name, int[] value) {
-            return this.constant(name, value, Integer.class);
-        }
-
-        Output<Float> constant(String name, float value) {
-            return this.constant(name, value, Float.class);
-        }
-
-        private <T> Output<T> binaryOp(String type, Output<T> in1, Output<T> in2) {
-            return g.opBuilder(type, type).addInput(in1).addInput(in2).build().<T>output(0);
-        }
-
-        private <T, U, V> Output<T> binaryOp3(String type, Output<U> in1, Output<V> in2) {
-            return g.opBuilder(type, type).addInput(in1).addInput(in2).build().<T>output(0);
-        }
-        private Graph g;
-    }*/
-
-/*    private static List<String> readAllLinesOrExit(Path path) {
-        try {
-            return Files.readAllLines(path, Charset.forName("UTF-8"));
-        } catch (IOException e) {
-            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
-            System.exit(0);
-        }
-        return null;
-    }*/
-
-
-
-/*    public float detect(Bitmap bitmap) {
-
-        TensorFlowInferenceInterface inferenceInterface =
-                new TensorFlowInferenceInterface(assetManager, modelFilename);
-
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        for (int i = 0; i < intValues.length; ++i) {
-            byteValues[i * 3 + 2] = (byte) (intValues[i] & 0xFF);
-            byteValues[i * 3 + 1] = (byte) ((intValues[i] >> 8) & 0xFF);
-            byteValues[i * 3 + 0] = (byte) ((intValues[i] >> 16) & 0xFF);
-        }
-
-        inferenceInterface.feed(inputName, byteValues, 1, inputSize, inputSize, 3);
-        inferenceInterface.run(outputNames, logStats);
-
-        outputLocations = new float[MAX_RESULTS * 4];
-        outputScores = new float[MAX_RESULTS];
-        outputClasses = new float[MAX_RESULTS];
-        outputNumDetections = new float[1];
-        inferenceInterface.fetch(outputNames[0], outputLocations);
-        inferenceInterface.fetch(outputNames[1], outputScores);
-        inferenceInterface.fetch(outputNames[2], outputClasses);
-        inferenceInterface.fetch(outputNames[3], outputNumDetections);
-        return outputClasses[0];
-    }*/
 }
